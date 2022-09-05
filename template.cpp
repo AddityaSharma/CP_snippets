be064e00e27c930ca39afec808c597db8a6966da
Adi_Anant <72218536+AddityaSharma@users.noreply.github.com>
2022-09-05 22:50:39 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -79,3 +79,19 @@ int main() {
  
     return 0; 
 } 
+
+
+
+/////////////////////////////////////////////////Kartik Arora Helper Functions///////////////////////////////////////////////////
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fr(a,b) for(int i = a; i < b; i++)
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+#define prDouble(x) cout << fixed << setprecision(10) << x
+#define triplet pair<ll,pair<ll,ll>>
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
